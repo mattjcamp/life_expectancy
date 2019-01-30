f60f5770f05462fc4f4fbc684bd71b876a755591
MattjCamp <mjcampbell74@gmail.com>
2019-01-30 16:31:56 -0500

    added shiny app

--- life_expectancy_web_app/app.R
+++ life_expectancy_web_app/app.R
@@ -8,12 +8,26 @@
 #
 
 library(shiny)
+library(tidyverse)
+
+load("expectancy.rdata")
+
+expectancy <-
+  expectancy %>%
+  filter(!is.na(life_expectancy))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Life Expectancy Data"),
+    
+    p("Life expectancy data was downloaded from data.gov. This 
+      dataset contains death rates and life-expectancy statistics 
+      from 1900 to 2015. Exploratory analysis was conducted on this 
+      dataset. In the visualization below, you can see all life expectancy 
+      rates in the dataset and adjust the number of bins used to present the 
+      frequency."),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -32,16 +46,26 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw 
+# a histogram
+
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        # generate bins based on 
+        # input$bins from ui.R
+       life_expectancy  <- expectancy$life_expectancy
+        
+        bins <- seq(min(life_expectancy), 
+                    max(life_expectancy), 
+                    length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        # draw the histogram with the 
+        # specified number of bins
+        hist(life_expectancy, 
+             breaks = bins, 
+             col = 'lightblue', 
+             border = 'white')
     })
 }
 
